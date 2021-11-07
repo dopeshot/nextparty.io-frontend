765c6e2599d306d90b562380246ebf65ec6be5c4
Lars Flieger <a.purenetwork@gmail.com>
2021-11-07 20:53:02 +0100

    Fix bug with floating button "play"

--- src/pages/SetDetails/SetDetails.tsx
+++ src/pages/SetDetails/SetDetails.tsx
@@ -1,4 +1,4 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonToolbar } from "@ionic/react"
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonRouterLink, IonToolbar } from "@ionic/react"
 import { useParams } from "react-router"
 import { TaskListItem, TaskType } from "../../components/TaskListItem/TaskListItem"
 
@@ -19,13 +19,17 @@ export const SetDetails: React.FC = () => {
                 </ IonToolbar>
             </IonHeader>
             <IonContent>
-                <div className="container">
-                    <div className="fixed bottom-4" style={{"width": "inherit"}}>
-                        <a href="/game" className="flex justify-center items-baseline bg-white rounded-lg py-4">
-                            <i className="fas fa-play text-black mr-3"></i>
-                            <span className="text-black font-bold">Spielen</span>
-                        </a>
+                <div className="fixed bottom-0 w-full">
+                    <div className="h-32 bg-gradient-to-t from-black">
+                        <div className="container h-full flex flex-col justify-center">
+                            <IonRouterLink routerLink="/game" className="flex justify-center items-baseline bg-white rounded-lg py-4">
+                                <i className="fas fa-play text-black mr-3"></i>
+                                <span className="text-black font-bold">Spielen</span>
+                            </IonRouterLink>
+                        </div>
                     </div>
+                </div>
+                <div className="container">
                     <div className="flex flex-col justify-end h-36 mb-16">
                         <h1 className="text-3xl mb-2">Love Set</h1>
                         <p className="text-gray-400 mb-5">by Cabcon</p>
