905693d3f3e4ea328847029b57cd899acd98296e
Lars Flieger <a.purenetwork@gmail.com>
2021-11-08 20:28:39 +0100

    Fix bug with wrong vertical alignment and update List view for taskListItem

--- src/components/TaskListItem/TaskListItem.tsx
+++ src/components/TaskListItem/TaskListItem.tsx
@@ -10,10 +10,12 @@ export const TaskListItem: React.FC<{
     content: string
 }> = ({ type, content }) => {
     return (
-        <IonItem button onClick={() => { console.log("hello") }} className="flex items-center">
-            <p className={type == TaskType.TRUTH ? "truth-label" : "dare-label"}>{type}</p>
-            <p className="flex-grow ml-1 mr-3">{content}</p>
-            <i className="fas fa-ellipsis-v text-darkgray"></i>
+        <IonItem button onClick={() => console.log("Press")} style={{"--min-height": "auto"}} className="rounded-lg my-1">
+            <div className="flex items-center w-full py-2">
+                <p className={type == TaskType.TRUTH ? "truth-label" : "dare-label"}>{type}</p>
+                <p className="flex-grow ml-1 mr-3">{content}</p>
+                <i className="fas fa-ellipsis-v text-darkgray"></i>
+            </div>
         </IonItem>
     )
 }
