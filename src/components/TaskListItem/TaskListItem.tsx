6174d42fa161d08a90a9f95b6a47fc3fc558f4eb
Lars Flieger <a.purenetwork@gmail.com>
2021-11-08 20:55:34 +0100

    Add action sheet for tasks

--- src/components/TaskListItem/TaskListItem.tsx
+++ src/components/TaskListItem/TaskListItem.tsx
@@ -1,4 +1,5 @@
-import { IonItem, IonLabel } from "@ionic/react"
+import { IonItem, IonLabel, useIonActionSheet } from "@ionic/react"
+import { flag, flagOutline, trash } from "ionicons/icons"
 
 export enum TaskType {
     TRUTH = "W",
@@ -9,8 +10,17 @@ export const TaskListItem: React.FC<{
     type: TaskType,
     content: string
 }> = ({ type, content }) => {
+
+    const [present] = useIonActionSheet()
+
     return (
-        <IonItem button onClick={() => console.log("Press")} style={{"--min-height": "auto"}} className="rounded-lg my-1">
+        <IonItem button onClick={() => {
+            present({
+                buttons: [{text: 'Löschen', icon: trash},{ text: 'Melden', icon:flag, handler: () => { console.log("Report") } }],
+                header: content
+            })
+        }
+        } style={{ "--min-height": "auto" }} className="rounded-lg my-1">
             <div className="flex items-center w-full py-2">
                 <p className={type == TaskType.TRUTH ? "truth-label" : "dare-label"}>{type}</p>
                 <p className="flex-grow ml-1 mr-3">{content}</p>
