c0f52029b6f9cc761ce4d7d624ca66cd91c84b5c
Lars Flieger <a.purenetwork@gmail.com>
2021-11-15 12:01:26 +0100

    Fix bug where toggle was displayed wrong

--- src/components/PlayerInput/PlayerInput.tsx
+++ src/components/PlayerInput/PlayerInput.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonButtons, IonIcon, IonInput, IonItem, IonToggle } from '@ionic/react';
+import { IonButton, IonButtons, IonIcon, IonInput, IonItem, IonLabel, IonToggle } from '@ionic/react';
 import { closeOutline } from 'ionicons/icons';
 import { useActions, useAppState } from '../../overmind';
 import { Gender, Player, playerNameLength } from '../../overmind/players/state';
@@ -8,10 +8,9 @@ export const PlayerInput: React.FC<{player: Player}> = ({ player }) => {
     const { deletePlayer, updatePlayerName, togglePlayerGender } = useActions().players
 
     return (<IonItem lines="none" className="bg-itemgrey rounded-lg mb-3 hover:bg-itemactivegrey focus:bg-itemactivegrey">
-        <IonToggle className="pl-3" mode="ios" slot="start" checked={player.gender === Gender.MALE ? false : true} onIonChange={(event: CustomEvent) => {
+        <IonToggle className="pl-3" mode="ios" slot="start" checked={player.gender !== Gender.MALE ? false : true} onIonChange={(event: CustomEvent) => {
             togglePlayerGender(player.id)
         }}>
-
         </IonToggle>
         <IonInput placeholder="Enter a name" maxlength={playerNameLength} value={player.name} onIonChange={(event: CustomEvent) => {
             updatePlayerName({id: player.id, name: event.detail.value})
