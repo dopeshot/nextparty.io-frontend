5a54bbd5b27d7bfcde4d2568b37638266573604e
Lars Flieger <a.purenetwork@gmail.com>
2021-11-17 14:21:08 +0100

    Fixed bug where input was not visible focused on desktop

--- src/components/PlayerInput/PlayerInput.tsx
+++ src/components/PlayerInput/PlayerInput.tsx
@@ -6,7 +6,7 @@ import { Gender, Player, playerNameLength } from '../../overmind/players/state';
 export const PlayerInput: React.FC<{player: Player, isAllowedToDelete: boolean}> = ({ player, isAllowedToDelete }) => {
     const { deletePlayer, updatePlayerName, togglePlayerGender } = useActions().players
 
-    return (<IonItem lines="none" className="bg-itemgrey rounded-lg mb-3 hover:bg-itemactivegrey focus:bg-itemactivegrey">
+    return (<IonItem lines="none" className="bg-itemgrey rounded-lg mb-3 hover:bg-itemactivegrey focus-within:bg-itemactivegrey">
         <IonToggle className="pl-3" mode="ios" slot="start" checked={player.gender !== Gender.MALE ? false : true} onIonChange={(event: CustomEvent) => {
             togglePlayerGender(player.id)
         }}>
