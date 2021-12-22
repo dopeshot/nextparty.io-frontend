678a14238f1d7c7723885caea9d5b71becb84a99
Lars Flieger <a.purenetwork@gmail.com>
2021-12-22 18:35:09 +0100

    Add basic layout

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,7 +1,9 @@
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonToggle } from '@ionic/react';
-import { PrimaryButton } from '../../components/Buttons/PrimaryButton';
+import { faCheckCircle, faCircle, faGlassCheers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IonContent, IonPage } from '@ionic/react';
+import example from '../../assets/example.png';
 import { useActions, useAppState } from '../../overmind';
+import { StartGameErrors } from '../../overmind/game/state';
 
 export const Game: React.FC = () => {
   const { game: {
@@ -17,36 +19,76 @@ export const Game: React.FC = () => {
 
   return (
     <IonPage className="bg-background-black">
-      <IonHeader className="ion-no-border container my-4">
-        <h1 className="text-3xl text-white font-bold">Game</h1>
-      </IonHeader>
       <IonContent>
-        <div className="container">
-          {isPossibleToPlay().status && <PrimaryButton link="/game/ingame" className="bg-white" icon={faPlay}>Play</PrimaryButton>}
-          <IonList>
-            <IonItem>
-              <IonLabel>{isDeveloper ? 'You are a developer' : 'You are not a developer'}</IonLabel>
-              <IonToggle checked={isDeveloper} onIonChange={e => toggleDeveloper()} />
-            </IonItem>
-          </IonList>
-          <IonList>
-            <IonListHeader>
-              <IonLabel>Players</IonLabel>
-            </IonListHeader>
-            {players && players.map(player =>
-              <IonItem key={player.id}><IonLabel>{JSON.stringify(player)}</IonLabel></IonItem>
-            )}
-          </IonList>
-          <IonList>
-            <IonListHeader>
-              <IonLabel>Set</IonLabel>
-            </IonListHeader>
-            {set && set.tasks.map(playTask =>
-              <IonItem key={playTask._id}><IonLabel>({playTask.currentPlayerGender}): {playTask.type}: {playTask.message} | {JSON.stringify(playTask.requires)}</IonLabel></IonItem>
-            )}
-          </IonList>
+        <div className='flex flex-col justify-between h-full'>
+          <div className="ion-no-border bg-cover mb-8" style={{ backgroundImage: `url(${example})` }}>
+            <div className="bg-gradient-to-t from-background-black w-full h-full">
+              <div className="container text-center">
+                <FontAwesomeIcon className="text-white text-6xl mt-14 mb-6" icon={faGlassCheers} />
+                <h1 className="text-3xl text-white font-bold mb-2">Truth or Dare</h1>
+                <p className="text-lightgrey">nextparty.io</p>
+              </div>
+            </div>
+          </div>
+          <div className='container bg-gray-900'>
+            <FontAwesomeIcon className="text-white" icon={isPossibleToPlay().errors.includes(StartGameErrors.PLAYERS) ? faCircle : faCheckCircle} />
+            <p>Players</p>
+            <FontAwesomeIcon className="text-white" icon={isPossibleToPlay().errors.includes(StartGameErrors.SET) ? faCircle : faCheckCircle} />
+            <p>Set</p>
+          </div>
+          <div className='container bg-gray-900'>
+            <FontAwesomeIcon className="text-white" icon={isPossibleToPlay().status ? faCheckCircle : faCircle} />
+            <p>Play</p>
+            {/* Play is a link to link="/game/ingame" */}
+          </div>
         </div>
       </IonContent>
     </IonPage>
   )
 }
+
+
+/**
+ * OLD PAGE: 
+ * 
+<div className="container">
+  {isPossibleToPlay().status && (
+    <PrimaryButton link="/game/ingame" className="bg-white" icon={faPlay}>
+      Play
+    </PrimaryButton>
+  )}
+  <IonList>
+    <IonItem>
+      <IonLabel>
+        {isDeveloper ? "You are a developer" : "You are not a developer"}
+      </IonLabel>
+      <IonToggle checked={isDeveloper} onIonChange={(e) => toggleDeveloper()} />
+    </IonItem>
+  </IonList>
+  <IonList>
+    <IonListHeader>
+      <IonLabel>Players</IonLabel>
+    </IonListHeader>
+    {players &&
+      players.map((player) => (
+        <IonItem key={player.id}>
+          <IonLabel>{JSON.stringify(player)}</IonLabel>
+        </IonItem>
+      ))}
+  </IonList>
+  <IonList>
+    <IonListHeader>
+      <IonLabel>Set</IonLabel>
+    </IonListHeader>
+    {set &&
+      set.tasks.map((playTask) => (
+        <IonItem key={playTask._id}>
+          <IonLabel>
+            ({playTask.currentPlayerGender}): {playTask.type}:{" "}
+            {playTask.message} | {JSON.stringify(playTask.requires)}
+          </IonLabel>
+        </IonItem>
+      ))}
+  </IonList>
+</div>;
+ */
