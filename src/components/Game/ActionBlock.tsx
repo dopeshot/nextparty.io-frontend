74445ba6f11bc2e9d0999edccf80a15b646ef90b
Lars Flieger <a.purenetwork@gmail.com>
2021-12-22 20:45:45 +0100

    Add hover logic

--- src/components/Game/ActionBlock.tsx
+++ src/components/Game/ActionBlock.tsx
@@ -11,7 +11,7 @@ export const ActionBlock: React.FC<ActionBlockType> = ({ routerLink, isReady, ch
     const statusIcon = isReady ? faCheckCircle : faCircle
 
     return <IonRouterLink routerLink={routerLink} className="">
-        <div className='bg-white cursor-pointer rounded-lg h-16 w-full flex items-center mb-4'>
+        <div className='bg-white cursor-pointer rounded-lg h-16 w-full flex items-center mb-4 transition-colors hover:bg-dare-green'>
             <FontAwesomeIcon icon={statusIcon} className="text-black ml-6 mr-4" />
             <span className="text-black font-bold">{children}</span>
             <FontAwesomeIcon icon={faChevronRight} className="ml-auto text-black mr-6" />
