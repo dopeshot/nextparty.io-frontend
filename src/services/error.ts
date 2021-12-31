18f57bfccb54c59b177652f918bb2fdcee594305
Lars Flieger <a.purenetwork@gmail.com>
2021-12-31 11:56:23 +0100

    Ref enums

--- src/services/error.ts
+++ src/services/error.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { HttpStatus } from "../enums/http-status";
+import { HttpStatus } from "../shared/enums/http-status";
 
 export const formatErrors = (data: string | string[]): string[] => {
     if (typeof data === 'string') {
