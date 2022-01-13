f9e6758814cd9238e500094b45228ad8730bdc74
Lars Flieger <a.purenetwork@gmail.com>
2022-01-13 16:01:29 +0100

    Fix error with unit tests

--- cypress/integration/unit-game-components.spec.ts
+++ cypress/integration/unit-game-components.spec.ts
@@ -1,8 +1,27 @@
-describe("getPossibleTasks", () => {
-  before(() => {
-    // expect(getPossibleTasks).to.be.a("function");
-  });
-  it("");
+import { TaskType } from "../../src/overmind/game/state";
+import { Gender } from "../../src/overmind/players/state";
+import { getPossibleTasks } from "../../src/services/game/GameComponents";
+
+// Docs here: https://docs.cypress.io/guides/references/assertions#BDD-Assertions
+// more here: https://github.com/chaijs/chai
+describe("getPossibleTasks Unit tests", () => {
+	before(() => {
+		expect(getPossibleTasks).to.be.a("function");
+	});
+
+	it('should be empty if tasks are empty', () => {
+		expect(getPossibleTasks([], {
+			id: 0,
+			gender: Gender.FEMALE,
+			name: "Michael"
+		}, TaskType.TRUTH)).to.be.an('array').that.is.empty
+	})
+
+	it('Just a random test', () => {
+		expect(true).to.be.true
+	})
+
 });
 
-export {};
+export { };
+
