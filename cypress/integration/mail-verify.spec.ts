e86fab6bf3caaf82f98625e564ab53068e4fa54c
Lars Flieger <a.purenetwork@gmail.com>
2022-01-13 09:20:25 +0100

    Add mail verify tests

--- cypress/integration/mail-verify.spec.ts
+++ cypress/integration/mail-verify.spec.ts
@@ -4,16 +4,14 @@ describe('Mail Verify', () => {
     })
     it('should display success message when code is correct', () => {
         cy.getMail("success")
-        cy.visit('/account/verify-account?code=1234')
-
-
+        cy.visit('/account/verify-account/1234')
+        cy.contains("Success").should("be.visible")
     })
 
     it('should display fail message when code is incorrect', () => {
-        cy.getMail("success")
-        cy.visit('/account/verify-account?code=1234')
-
-
+        cy.getMail("fail")
+        cy.visit('/account/verify-account/1234')
+        cy.contains("Failed").should("be.visible")
     })
 })
 
