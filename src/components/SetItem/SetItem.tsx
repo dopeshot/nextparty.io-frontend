3f55ef22a432cfa3727e8792874c435a1a534bba
Lars Flieger <a.purenetwork@gmail.com>
2022-01-18 00:29:19 +0100

    Update preview image based on category

--- src/components/SetItem/SetItem.tsx
+++ src/components/SetItem/SetItem.tsx
@@ -1,5 +1,4 @@
 import { IonItem } from '@ionic/react';
-import gradient from '../../assets/example.png';
 import { SetCategory } from '../../shared/types/SetCategory';
 import { DareLabel } from './DareLabel';
 import { TruthLabel } from './TruthLabel';
@@ -19,7 +18,7 @@ export const SetItem: React.FC<SetItemProps> = ({ name, author, truthCount, dare
 	return (
 		<IonItem data-cy={dataCy} lines="none" routerLink={link ?? '#'} onClick={onClick} className="mb-5 rounded-lg">
 			<div className="flex items-center">
-				<img src={gradient} alt={`${name} by ${author}`} className={`object-cover rounded-lg ${author ? "w-24 h-24" : "w-20 h-20"} mr-2`} />
+				<img src={`${process.env.REACT_APP_PUBLIC_URL}/assets/themes/${category}.svg`} alt={`${name} by ${author}`} className={`object-cover rounded-lg ${author ? "w-24 h-24" : "w-20 h-20"} mr-2`} />
 				<div className="flex flex-col">
 					<h3 className={`text-light-500 font-bold ${author ? "text-xl" : "text-lg mb-2"}`}>{name}</h3>
 					{author && <p className="text-light-600 mb-3">by {author}</p>}
