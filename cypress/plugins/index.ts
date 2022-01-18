18d4e10061d14fc5650150a45cab1e9a0c832f69
Lars Flieger <a.purenetwork@gmail.com>
2022-01-18 19:44:27 +0100

    Fix ts error

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -15,10 +15,22 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
+// const browserify = require('@cypress/browserify-preprocessor')
 module.exports = (on: Cypress.PluginEvents, config: Cypress.ConfigOptions) => {
   require('@cypress/code-coverage/task')(on, config)
+
+  // MC: This is the attempt to add coverage without starting the application. Tests are not working at the cypress examples: https://github.com/cypress-io/code-coverage/blob/4cfcc9ad968e6ad628b1abb9e81a8c504833b65d/examples/unit-tests-ts/README.md
+  // const options = browserify.defaultOptions
+  // options.browserifyOptions.extensions.push('.ts')
+  // options.typescript = require.resolve('typescript')
+  // // transform[1][1] is "babelify"
+  // // so we just add our code instrumentation plugin to the list
+  // options.browserifyOptions.transform[1][1].plugins.push('babel-plugin-istanbul')
+
+  // on('file:preprocessor', browserify(options))
   return config
 }
 
+
 export { }
+
