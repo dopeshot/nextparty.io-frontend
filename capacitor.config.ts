0969a12384ab37cdddc6d040e0bd0260b5d5703f
coffemakingtoaster <mh340@hdm-stuttgart.de>
2022-01-24 20:50:21 +0100

    overwrite user agent

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'truth-or-dare-frontend',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "android.nextparty.io",
+  appName: "nextparty",
+  webDir: "build",
+  bundledWebRuntime: false,
+  overrideUserAgent:
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
 };
 
 export default config;
