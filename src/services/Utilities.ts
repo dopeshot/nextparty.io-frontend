a922b393880e4acc922af40ee76851cd4b8848c3
coffemakingtoaster <mh340@hdm-stuttgart.de>
2022-01-24 20:12:27 +0100

    remove findall as this crashes the mobile app

--- src/services/Utilities.ts
+++ src/services/Utilities.ts
@@ -1,50 +1,49 @@
 export function lowerCaseFirstLetter(string: string): string {
-    return string.charAt(0).toLowerCase() + string.slice(1)
+  return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
 export function replaceStringWithIcon(string: string): string {
-    return string
-        .replaceAll('@a', '👤')
-        .replaceAll('@m', '👨')
-        .replaceAll('@f', '👩')
+  return string.replace(/@a/g, "👤").replace(/@m/g, "👨").replace(/@f/g, "👩");
 }
 
 export function replaceIconWithString(string: string): string {
-    return string
-        .replaceAll('👤', '@a')
-        .replaceAll('👨', '@m')
-        .replaceAll('👩', '@f')
+  return string.replace(/👤/g, "@a").replace(/👨/g, "@m").replace(/👩/g, "@f");
 }
 
 export function replaceCurrentPlayerStringWithIcon(string: string): string {
-    return string
-        .replaceAll('@ca', '👤')
-        .replaceAll('@cm', '👨')
-        .replaceAll('@cf', '👩')
+  return string
+    .replace(/@ca/g, "👤")
+    .replace(/@cm/g, "👨")
+    .replace(/@cf/g, "👩");
 }
 
 export const parseJwt = (token: string) => {
-    const base64Url = token.split('.')[1]
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join(''))
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+      .join("")
+  );
 
-    return JSON.parse(jsonPayload)
-}
+  return JSON.parse(jsonPayload);
+};
 
-const titleSuffix = ' | Truth or Dare'
+const titleSuffix = " | Truth or Dare";
 
 export const setSeoTitle = (title: string, displaySuffix = true) => {
-    document.title = `${title}${displaySuffix ? titleSuffix : ''}`
-}
+  document.title = `${title}${displaySuffix ? titleSuffix : ""}`;
+};
 
 export function countGenderOccurrences(string: string): {
-    male: number,
-    female: number,
-    any: number
+  male: number;
+  female: number;
+  any: number;
 } {
-    return {
-        male: string.split('@m').length - 1,
-        female: string.split('@f').length - 1,
-        any: string.split('@a').length - 1
-    }
+  return {
+    male: string.split("@m").length - 1,
+    female: string.split("@f").length - 1,
+    any: string.split("@a").length - 1,
+  };
 }
--- src/services/game/GameComponents.ts
+++ src/services/game/GameComponents.ts
@@ -1,133 +1,189 @@
-import { PlayerGenderCount, PlayTask } from "../../overmind/game/state"
-import { Gender, Player } from "../../overmind/players/state"
-import { TaskCurrentPlayerGender } from "../../shared/types/TaskCurrentPlayerGender"
-import { TaskPlayerGender } from "../../shared/types/TaskPlayerGender"
-import { TaskType } from "../../shared/types/TaskType"
-import { countPlayedByPlayer, genderToTaskCurrentPlayerGender, shuffleArray } from "./GameUtilities"
+import { PlayerGenderCount, PlayTask } from "../../overmind/game/state";
+import { Gender, Player } from "../../overmind/players/state";
+import { TaskCurrentPlayerGender } from "../../shared/types/TaskCurrentPlayerGender";
+import { TaskPlayerGender } from "../../shared/types/TaskPlayerGender";
+import { TaskType } from "../../shared/types/TaskType";
+import {
+  countPlayedByPlayer,
+  genderToTaskCurrentPlayerGender,
+  shuffleArray,
+} from "./GameUtilities";
 
-export const getPossibleTasks = (tasks: PlayTask[], player: Player, pickedTaskType: TaskType | null) => {
-    return tasks.filter(task =>
-        (task.type === pickedTaskType || !pickedTaskType) &&
-        (
-            task.currentPlayerGender === TaskCurrentPlayerGender.ANYONE ||
-            task.currentPlayerGender === genderToTaskCurrentPlayerGender(player.gender) ||
-            player.gender === Gender.DIVERS
-        )
-    )
-}
+export const getPossibleTasks = (
+  tasks: PlayTask[],
+  player: Player,
+  pickedTaskType: TaskType | null
+) => {
+  return tasks.filter(
+    (task) =>
+      (task.type === pickedTaskType || !pickedTaskType) &&
+      (task.currentPlayerGender === TaskCurrentPlayerGender.ANYONE ||
+        task.currentPlayerGender ===
+          genderToTaskCurrentPlayerGender(player.gender) ||
+        player.gender === Gender.DIVERS)
+  );
+};
 
-export const getFillableTasks = (tasks: PlayTask[], player: Player, playerGenderCount: PlayerGenderCount) => {
-    const reducedGenders = { ...playerGenderCount }
-    if (!reducedGenders) return []
-    reducedGenders[player.gender]--
+export const getFillableTasks = (
+  tasks: PlayTask[],
+  player: Player,
+  playerGenderCount: PlayerGenderCount
+) => {
+  const reducedGenders = { ...playerGenderCount };
+  if (!reducedGenders) return [];
+  reducedGenders[player.gender]--;
 
-    const fillableTasks = tasks.filter((task) => {
-        // Check if there are enough people to play the task at all
-        if (Object.values(reducedGenders).reduce((sum, value) => sum + value) - Object.values(task.requires).reduce((sum, value) => sum + value) < 0) {
-            return false
-        }
-        // Check if there is no need to fill in divers people
-        if ((task.requires.male <= reducedGenders.male && task.requires.female <= reducedGenders.female)) {
-            return true
-        }
-        // Fill in divers players
-        else {
-            let divers = reducedGenders.divers
-            if (task.requires.male > reducedGenders.male) {
-                divers -= task.requires.male - reducedGenders.male
-            }
-            if (task.requires.female > reducedGenders.female) {
-                divers -= task.requires.female - reducedGenders.female
-            }
-            return divers >= 0
-        }
-    })
-    return fillableTasks
-}
+  const fillableTasks = tasks.filter((task) => {
+    // Check if there are enough people to play the task at all
+    if (
+      Object.values(reducedGenders).reduce((sum, value) => sum + value) -
+        Object.values(task.requires).reduce((sum, value) => sum + value) <
+      0
+    ) {
+      return false;
+    }
+    // Check if there is no need to fill in divers people
+    if (
+      task.requires.male <= reducedGenders.male &&
+      task.requires.female <= reducedGenders.female
+    ) {
+      return true;
+    }
+    // Fill in divers players
+    else {
+      let divers = reducedGenders.divers;
+      if (task.requires.male > reducedGenders.male) {
+        divers -= task.requires.male - reducedGenders.male;
+      }
+      if (task.requires.female > reducedGenders.female) {
+        divers -= task.requires.female - reducedGenders.female;
+      }
+      return divers >= 0;
+    }
+  });
+  return fillableTasks;
+};
 
 export const getUnplayedOverall = (tasks: PlayTask[]) => {
-    return tasks.filter(task => task.playedBy.length === 0)
-}
+  return tasks.filter((task) => task.playedBy.length === 0);
+};
 
 export const getUnplayedByMe = (tasks: PlayTask[], player: Player) => {
-    return tasks.filter(task => !task.playedBy.includes(player.id))
-}
+  return tasks.filter((task) => !task.playedBy.includes(player.id));
+};
 
 export const getLeastPlayedByMe = (tasks: PlayTask[], player: Player) => {
-    const sortedTasks = tasks.sort((a, b) => countPlayedByPlayer(a.playedBy, player) - countPlayedByPlayer(b.playedBy, player))
-    return sortedTasks.filter(task => countPlayedByPlayer(task.playedBy, player) === countPlayedByPlayer(sortedTasks[0].playedBy, player))
-}
+  const sortedTasks = tasks.sort(
+    (a, b) =>
+      countPlayedByPlayer(a.playedBy, player) -
+      countPlayedByPlayer(b.playedBy, player)
+  );
+  return sortedTasks.filter(
+    (task) =>
+      countPlayedByPlayer(task.playedBy, player) ===
+      countPlayedByPlayer(sortedTasks[0].playedBy, player)
+  );
+};
 
 export const getLeastPlayedOverall = (tasks: PlayTask[]) => {
-    return tasks.sort((a, b) => (a.playedBy.length - b.playedBy.length))[0]
-}
+  return tasks.sort((a, b) => a.playedBy.length - b.playedBy.length)[0];
+};
 
-export const fillPlayersIntoMessage = (players: Player[], playTask: PlayTask, currentPlayer: Player) => {
-    const playersWithoutCurrent = [...players].filter(player => player.id !== currentPlayer.id)
-    // This shuffle needs to be there to make sure that not all the same divers get merged later on
-    const playerNamesByGender = shuffleArray(playersWithoutCurrent).reduce<{
-        male: string[],
-        female: string[],
-        divers: string[]
-    }>((result, player) => {
-        switch (player.gender) {
-            case Gender.MALE:
-                result.male.push(player.name)
-                break
-            case Gender.FEMALE:
-                result.female.push(player.name)
-                break
-            case Gender.DIVERS:
-                result.divers.push(player.name)
-                break
-        }
-        return result
-    }, {
-        male: [],
-        female: [],
-        divers: []
-    })
-
-    // Merge divers into male and female where needed, then distribute the rest
-    for (let i = 0; i < playTask.requires.male - playerNamesByGender.male.length; i++) {
-        playerNamesByGender.male.push(playerNamesByGender.divers.pop()!)
+export const fillPlayersIntoMessage = (
+  players: Player[],
+  playTask: PlayTask,
+  currentPlayer: Player
+) => {
+  const playersWithoutCurrent = [...players].filter(
+    (player) => player.id !== currentPlayer.id
+  );
+  // This shuffle needs to be there to make sure that not all the same divers get merged later on
+  const playerNamesByGender = shuffleArray(playersWithoutCurrent).reduce<{
+    male: string[];
+    female: string[];
+    divers: string[];
+  }>(
+    (result, player) => {
+      switch (player.gender) {
+        case Gender.MALE:
+          result.male.push(player.name);
+          break;
+        case Gender.FEMALE:
+          result.female.push(player.name);
+          break;
+        case Gender.DIVERS:
+          result.divers.push(player.name);
+          break;
+      }
+      return result;
+    },
+    {
+      male: [],
+      female: [],
+      divers: [],
     }
-    for (let i = 0; i < playTask.requires.female - playerNamesByGender.female.length; i++) {
-        playerNamesByGender.female.push(playerNamesByGender.divers.pop()!)
+  );
+
+  // Merge divers into male and female where needed, then distribute the rest
+  for (
+    let i = 0;
+    i < playTask.requires.male - playerNamesByGender.male.length;
+    i++
+  ) {
+    playerNamesByGender.male.push(playerNamesByGender.divers.pop()!);
+  }
+  for (
+    let i = 0;
+    i < playTask.requires.female - playerNamesByGender.female.length;
+    i++
+  ) {
+    playerNamesByGender.female.push(playerNamesByGender.divers.pop()!);
+  }
+  for (let i = 0; i < playerNamesByGender.divers.length; i++) {
+    if (Math.random() - 0.5 <= 0) {
+      playerNamesByGender.male.push(playerNamesByGender.divers[i]);
+    } else {
+      playerNamesByGender.female.push(playerNamesByGender.divers[i]);
     }
-    for (let i = 0; i < playerNamesByGender.divers.length; i++) {
-        if (Math.random() - 0.5 <= 0) {
-            playerNamesByGender.male.push(playerNamesByGender.divers[i])
-        }
-        else {
-            playerNamesByGender.female.push(playerNamesByGender.divers[i])
-        }
-    }
+  }
 
-    // Shuffle again to have divers random 
-    playerNamesByGender.male = shuffleArray(playerNamesByGender.male)
-    playerNamesByGender.female = shuffleArray(playerNamesByGender.female)
-    // Create message
-    const message = playTask.message
-        .replaceAll(TaskPlayerGender.MALE, () => playerNamesByGender.male.pop()!)
-        .replaceAll(TaskPlayerGender.FEMALE, () => playerNamesByGender.female.pop()!)
-        .replaceAll(TaskPlayerGender.ANYONE, () => {
-            if (playerNamesByGender.male.length > 0 && playerNamesByGender.female.length > 0)
-                return Math.random() - 0.5 < 0 ? playerNamesByGender.male.pop()! : playerNamesByGender.female.pop()!
-            else if (playerNamesByGender.male.length > 0)
-                return playerNamesByGender.male.pop()!
-            else
-                return playerNamesByGender.female.pop()!
-        })
+  // Shuffle again to have divers random
+  playerNamesByGender.male = shuffleArray(playerNamesByGender.male);
+  playerNamesByGender.female = shuffleArray(playerNamesByGender.female);
+  // Create message
+  const message = playTask.message
+    .replace(
+      new RegExp(TaskPlayerGender.MALE, "g"),
+      () => playerNamesByGender.male.pop()!
+    )
+    .replace(
+      new RegExp(TaskPlayerGender.FEMALE, "g"),
+      () => playerNamesByGender.female.pop()!
+    )
+    .replace(new RegExp(TaskPlayerGender.ANYONE, "g"), () => {
+      if (
+        playerNamesByGender.male.length > 0 &&
+        playerNamesByGender.female.length > 0
+      )
+        return Math.random() - 0.5 < 0
+          ? playerNamesByGender.male.pop()!
+          : playerNamesByGender.female.pop()!;
+      else if (playerNamesByGender.male.length > 0)
+        return playerNamesByGender.male.pop()!;
+      else return playerNamesByGender.female.pop()!;
+    });
 
-    return {
-        ...playTask,
-        message
-    }
-}
+  return {
+    ...playTask,
+    message,
+  };
+};
 
-export const countPossibleTasksForPlayer = (tasks: PlayTask[], player: Player, playerGenderCount: PlayerGenderCount): number => {
-    const possibleTasks = getPossibleTasks(tasks, player, null)
-    return getFillableTasks(possibleTasks, player, playerGenderCount).length
-}
-
+export const countPossibleTasksForPlayer = (
+  tasks: PlayTask[],
+  player: Player,
+  playerGenderCount: PlayerGenderCount
+): number => {
+  const possibleTasks = getPossibleTasks(tasks, player, null);
+  return getFillableTasks(possibleTasks, player, playerGenderCount).length;
+};
