ea3186762190ee0382c44e87937bc75f342ed38a
Max Herkenhoff <max@Maxs-MacBook-Pro.local>
2022-01-28 16:21:03 +0100

    fix issues with google login not updating page

--- src/components/Buttons/GoogleLoginButton.tsx
+++ src/components/Buttons/GoogleLoginButton.tsx
@@ -1,11 +1,21 @@
+import { useHistory } from 'react-router';
 import { useActions } from '../../overmind';
 
 
 export const GoogleLoginButton: React.FC = () => {
     const { loginWithGoogle } = useActions().profile
+    let disabled = false
+    const history = useHistory()
+    const login = async () => {
+        if (await loginWithGoogle()) {
+            history.replace('/account/profile')
+            return
+        }
+        disabled = true;
+    }
 
     return (
-        <button type="button"  onClick={loginWithGoogle} className={`flex justify-center items-center text-light-500 border border-light-500 border-solid ${ "transition hover:-translate-y-1 cursor-pointer"} rounded-lg py-3 mb-4 w-full`}>
+        <button type="button" onClick={login} className={`flex justify-center items-center text-light-500 border border-light-500 border-solid ${disabled ? "cursor-default bg-opacity-75 text-opacity-75" : "transition hover:-translate-y-1 cursor-pointer"} rounded-lg py-3 mb-4 w-full`}>
             <span className="text-light-500 font-bold">Continue with Google</span>
         </button>
     )
