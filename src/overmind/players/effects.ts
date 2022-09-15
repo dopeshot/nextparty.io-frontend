1c02b6a1b481aa4d060d69a81418fbea9e0625d8
Lars Flieger <a.purenetwork@gmail.com>
2022-09-16 00:07:41 +0200

    Rename key

--- src/overmind/players/effects.ts
+++ src/overmind/players/effects.ts
@@ -1,9 +1,13 @@
 import { Player } from "./state"
 
+const STORAGE_KEYS_PLAYERS = "wop_players"
+
 export const savePlayerState = (players: Player[]): void => {
-    localStorage.setItem("players", JSON.stringify(players))
+    localStorage.setItem(STORAGE_KEYS_PLAYERS, JSON.stringify(players))
 }
 
 export const loadPlayerState = (): Player[] => {
-    return JSON.parse(localStorage.getItem("players") ?? "[]") as Player[]
+    const playersFromLocalStorage = localStorage.getItem(STORAGE_KEYS_PLAYERS)
+
+    return playersFromLocalStorage ? JSON.parse(playersFromLocalStorage) : [] as Player[]
 }
